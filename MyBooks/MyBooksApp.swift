5af37b36a82e255946758797398a8b61bf8a8358
zhou.xlin <xlin173@foxmail.com>
2024-03-03 22:12:34 +0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Hope on 2024/3/3.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
