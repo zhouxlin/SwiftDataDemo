a20de6caefe4288d1497409b0108fd673742f440
zhou.xlin <xlin173@foxmail.com>
2024-03-10 22:54:49 +0800

    save code

--- MyBooks/BookListView.swift
+++ MyBooks/BookListView.swift
@@ -70,5 +70,7 @@ struct BookListView: View {
 }
 
 #Preview {
-    BookListView().modelContainer(for: Book.self, inMemory: true)
+    let preview = Preview(Book.self)
+    preview.addExamples(Book.sampleBooks)
+    return BookListView().modelContainer(preview.container)
 }
--- MyBooks/Preview Content/PreviewContainer.swift
+++ MyBooks/Preview Content/PreviewContainer.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftData
+
+struct Preview {
+    let container:ModelContainer
+    init(_ models: any PersistentModel.Type...) {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let schema = Schema(models)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Couldon't create preview container")
+        }
+    }
+    
+    func addExamples(_ examples: [any PersistentModel]) {
+        Task { @MainActor in // 加入异步任务，在主线程中进行
+            examples.forEach { example in
+                container.mainContext.insert(example)
+            }
+        }
+    }
+}
