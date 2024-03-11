367d2ff1817138da3500d6517ca4a9a0bb5e3aa3
zhou.xlin <xlin173@foxmail.com>
2024-03-11 22:22:43 +0800

    3rd course finished

--- MyBooks/BookListView.swift
+++ MyBooks/BookListView.swift
@@ -18,10 +18,11 @@ enum SortOrder: String, Identifiable, CaseIterable {
 }
 
 struct BookListView: View {
-    @Environment(\.modelContext) private var context
-    @Query(sort: (\Book.title)) private var books: [Book]
+
     @State private var createBook = false
     @State private var sortOrder = SortOrder.status
+    @State private var filter = ""
+    
     var body: some View {
         NavigationStack {
             Picker("", selection: $sortOrder) {
@@ -30,44 +31,8 @@ struct BookListView: View {
                 }
             }
             .buttonStyle(.bordered)
-            Group {
-                if (books.isEmpty) {
-                    ContentUnavailableView("Enter your first book", systemImage:"book.fill")
-                } else {
-                    List {
-                        ForEach(books) { book in
-                            NavigationLink {
-                                EditBookView(book: book)
-                            } label: {
-                                HStack (spacing: 10) {
-                                    book.icon
-                                    VStack(alignment: .leading){
-                                        Text(book.title).font(.title2)
-                                        Text(book.author).foregroundStyle(.secondary)
-                                        if let rating = book.rating {
-                                            HStack {
-                                                ForEach(1..<rating, id: \.self) {
-                                                    _ in
-                                                    Image(systemName: "star.fill")
-                                                        .imageScale(.small)
-                                                        .foregroundColor(.yellow)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        .onDelete { indexSet in
-                            indexSet.forEach{ index in
-                                let book = books[index]
-                                context.delete(book)
-                            }
-                        }
-                    }
-                    .listStyle(.plain)
-                }
-            }
+            BookList(sortOrder: sortOrder, filterString: filter)
+                .searchable(text: $filter, prompt: "filter on title or author")
             .navigationTitle("My Books")
             .toolbar{
                 Button{
